ee8ae9c2b7055e0a26ee838be7688072f183691a
nashibudou <cbrk1210458@gn.iwasaki.ac.jp>
2022-06-03 08:50:06 +0900

    STEP10

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
-from flask_login import UserMixin,LoginManager
+from flask_login import UserMixin,LoginManager,login_user,logout_user,login_required
 import os
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
@@ -22,6 +23,10 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(20),unique=True)
     password = db.Column(db.String(20))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route("/",methods=["GET"])
 def index():
@@ -67,6 +72,36 @@ def delete(id):
     db.session.delete(post)
     db.session.commit()
     return redirect("/")
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    if request.method == "POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+        user=User(username=username,password=generate_password_hash(password,method="sha256"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username=request.form.get("username")
+        password=request.form.get("password")
+        user=User.query.filter_by(username=username).first()
+        if check_password_hash(user.password,password):
+            login_user(user)
+            return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
 
 if __name__ == '__main__':
     app.run(debug=True)
